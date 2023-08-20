156a6dd6074492b0e0ab8e4b078847e23b36b8bc
Crashm <32693415+crashmoz@users.noreply.github.com>
2023-08-20 22:48:30 +0700

    Update firebaseDB.js

--- src/firebaseDB.js
+++ src/firebaseDB.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAFSlNSMI7Tfuhv9vBDQ4j3U9Dai4MSwls",
-    authDomain: "vue-firebase-crud-511fe.firebaseapp.com",
-    projectId: "vue-firebase-crud-511fe",
-    storageBucket: "vue-firebase-crud-511fe.appspot.com",
-    messagingSenderId: "55368239793",
-    appId: "1:55368239793:web:39a085dc5bf582f17cb090",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
 // const firebaseApp = firebase.initializeApp(firebaseConfig);
